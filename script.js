6c2fe53b495a937d940db61ee9973759c3396e09
Dili-tv <dilaraa.ly@gmail.com>
2026-02-25 22:11:14 +0100

    script.js

--- script.js
+++ script.js
@@ -22,11 +22,7 @@ const streamplan = [
   { tag: "Samstag", zeit: "18:00", titel: "Community Games" }
 ];
 
-const tage = [
-  "Sonntag","Montag","Dienstag",
-  "Mittwoch","Donnerstag","Freitag","Samstag"
-];
-
+const tage = ["Sonntag","Montag","Dienstag","Mittwoch","Donnerstag","Freitag","Samstag"];
 const heute = tage[new Date().getDay()];
 const container = document.getElementById("schedule");
 
@@ -47,16 +43,29 @@ streamplan.forEach(eintrag => {
   container.appendChild(card);
 });
 
-// SAKURA
+// REALISTISCHE SAKURA
 function createPetal() {
+
   const petal = document.createElement("div");
   petal.className = "petal";
+
   petal.style.left = Math.random() * window.innerWidth + "px";
-  petal.style.animationDuration = (6 + Math.random() * 6) + "s";
+  petal.style.animationDuration = (8 + Math.random() * 6) + "s";
+
+  petal.innerHTML = `
+    <svg viewBox="0 0 64 64" width="34" height="34">
+      <path d="M32 5 C40 5, 45 15, 32 22 C19 15, 24 5, 32 5 Z" fill="#ffb7d5"/>
+      <path d="M32 22 C45 15, 58 28, 45 38 C35 45, 30 35, 32 22 Z" fill="#ffc8e0"/>
+      <path d="M32 22 C30 35, 19 45, 9 38 C-4 28, 19 15, 32 22 Z" fill="#ffb7d5"/>
+      <circle cx="32" cy="28" r="4" fill="#ffd166"/>
+    </svg>
+  `;
 
   document.body.appendChild(petal);
 
-  setTimeout(() => petal.remove(), 12000);
+  setTimeout(() => {
+    petal.remove();
+  }, 14000);
 }
 
-setInterval(createPetal, 900)
+setInterval(createPetal, 1200);
